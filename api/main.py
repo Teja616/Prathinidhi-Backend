21042f08397c8fae34205c760a6a545b7694ed32
Vinay <vinayvelpula03@gmail.com>
2025-04-22 10:18:41 +0530

    added Request Module

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, Request
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from fastapi.middleware.cors import CORSMiddleware
 from jose import jwt, JWTError
